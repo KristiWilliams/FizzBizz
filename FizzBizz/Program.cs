6de893449c918c597d7d5a4eaa904d9fb95021b8
KristiWilliams <35853650+KristiWilliams@users.noreply.github.com>
2018-01-28 17:39:45 -0600

    Fiz
    Fiz Code

---
+++ FizzBizz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int currantnumber = 1; currantnumber < 101; currantnumber++)
+            {
+                if (0 == currantnumber % 15)
+                {
+                 Console.WriteLine("FizzBizz");
+                }
+                else if (0 == currantnumber % 5)
+                {
+                 Console.WriteLine("Buzz");
+                }
+                else if (0 == currantnumber % 3)
+                {
+                 Console.WriteLine("Fizz");
+                }
+                else
+                {
+                 Console.WriteLine(currantnumber);
+                }
+            }
+                 Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
